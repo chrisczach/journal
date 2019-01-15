ae915c3dc80aea3a24a3a5904b582fc670a551ef
Chris Czach <chrisczach@gmail.com>
2019-01-15 08:41:29 -0800

    .

--- src/main.js
+++ src/main.js
@@ -1,23 +1,28 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, Menu } = require('electron')
+const { app, BrowserWindow, dialog, Menu } = require('electron');
+const fs = require('fs');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600
-  } )
-  
+  });
+
   const template = [
     {
       label: 'File',
       submenu: [
         {
-          label: 'Open File'
+          label: 'Open File',
+          click() {
+            openFile();
+          },
+          accelerator: 'CmdOrCtrl+o'
         },
         {
           label: 'Open Folder'
@@ -54,24 +59,36 @@ function createWindow() {
     },
     {
       role: 'window',
-      submenu: [
-        { role: 'minimize' },
-        { role: 'close' }
-      ]
+      submenu: [{ role: 'minimize' }, { role: 'close' }]
     },
     {
       role: 'help',
       submenu: [
         {
           label: 'Learn More',
-          click() { require( 'electron' ).shell.openExternal( 'https://electronjs.org' ) }
+          click() {
+            require('electron').shell.openExternal('https://electronjs.org');
+          }
+        }
+      ]
+    },
+    {
+      label: 'Developer',
+      submenu: [
+        {
+          label: 'Toggle Developer Tools',
+          accelerator:
+            process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+          click() {
+            mainWindow.webContents.toggleDevTools();
+          }
         }
       ]
     }
-  ]
+  ];
 
-  if ( process.platform === 'darwin' ) {
-    template.unshift( {
+  if (process.platform === 'darwin') {
+    template.unshift({
       label: app.getName(),
       submenu: [
         { role: 'about' },
@@ -84,36 +101,33 @@ function createWindow() {
         { type: 'separator' },
         { role: 'quit' }
       ]
-    } )
+    });
 
     // Edit menu
     // @ts-ignore
-    template[ 2 ].submenu.push(
+    template[2].submenu.push(
       { type: 'separator' },
       {
         label: 'Speech',
-        submenu: [
-          { role: 'startspeaking' },
-          { role: 'stopspeaking' }
-        ]
+        submenu: [{ role: 'startspeaking' }, { role: 'stopspeaking' }]
       }
-    )
+    );
 
     // Window menu
-    template[ 4 ].submenu = [
+    template[4].submenu = [
       { role: 'close' },
       { role: 'minimize' },
       { role: 'zoom' },
       { type: 'separator' },
       { role: 'front' }
-    ]
+    ];
   }
 
-  const menu = Menu.buildFromTemplate( template )
-  Menu.setApplicationMenu( menu )
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 
   // and load the index.html of the app.
-  mainWindow.loadURL('http://localhost:3000')
+  mainWindow.loadURL('http://localhost:3000');
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -123,31 +137,52 @@ function createWindow() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', function() {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+//Open File
+function openFile() {
+  const files = dialog.showOpenDialog(mainWindow, {
+    properties: ['openFile'],
+    filters: [
+      {
+        name: 'Markdown',
+        extensions: ['md', 'markdown', 'txt']
+      }
+    ]
+  });
+
+  if (!files) return;
+  const file = files[0];
+  const fileContent = fs.readFileSync(file).toString();
+  console.log(fileContent);
+
+  //send file content to renderer
+  mainWindow.webContents.send('new-file', fileContent)
+}
